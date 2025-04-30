f5effe4a6b85510f7185f84bc5e228e532a83f77
David Y. Stephenson <david@davidystephenson.com>
2025-04-29 23:06:26 -0400

    Initialize chakra

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import { Table, Thead, Tbody, Tr, Th, Td } from '@chakra-ui/react'
+
+
 export default function App () {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <ChakraProvider>
+      <Table>
+        <Thead>
+          <Tr>
+            <Th>Name</Th>
+            <Th>Email</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          <Tr>
+            <Td>Dorothy Parker</Td>
+            <Td>dorothy@parker.com</Td>
+          </Tr>
+        </Tbody>
+      </Table>
+    </ChakraProvider>
   )
 }
