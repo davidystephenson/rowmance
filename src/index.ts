f165b7b2a168d70c3a85f479f5f93f05a7e368b8
David Y. Stephenson <david@davidystephenson.com>
2025-05-08 20:32:49 -0400

    Export FirstRowmance

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import Rowmance from './Rowmance'
 export default Rowmance
+export { default as FirstRowmance } from './FirstRowmance'
+export { default as Rowmance } from './Rowmance'
 export { default as LongRowmance } from './LongRowmance'
 export { default as RowmanceTable } from './RowmanceTable'
 export type { RowmanceProps, RowmanceTableProps } from './types'
