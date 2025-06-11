7bebc85e4bd77dbe53992e47d8b700e711854e30
David Y. Stephenson <david@davidystephenson.com>
2025-06-11 13:51:44 -0400

    Export RowmanceColumnProps

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,6 @@ export default Rowmance
 export { default as Rowmance } from './Rowmance'
 export { default as LongRowmance } from './LongRowmance'
 export { default as RowmanceTable } from './RowmanceTable'
-export type { RowmanceProps, RowmanceTableProps } from './types'
+export type {
+  RowmanceProps, RowmanceColumnProps, RowmanceTableProps
+} from './types'
