cbe90ed1750ca32b9fb5ecd7f9461ec5ed7d868e
David Y. Stephenson <david@davidystephenson.com>
2025-07-14 14:50:48 -0400

    Export useRowmanceContext

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export default Rowmance
 export { default as Rowmance } from './Rowmance'
 export { default as LongRowmance } from './LongRowmance'
 export { default as RowmanceTable } from './RowmanceTable'
+export { default as useRowmanceContext } from './useRowmanceContext'
 export type {
   RowmanceProps, RowmanceColumnProps, RowmanceTableProps
 } from './types'
---
+++ src/useRowmanceContext.ts
@@ -0,0 +1,5 @@
+import rowmanceContext from "./rowmanceContext"
+
+export default function useRowmanceContext() {
+  return rowmanceContext.use()
+}
